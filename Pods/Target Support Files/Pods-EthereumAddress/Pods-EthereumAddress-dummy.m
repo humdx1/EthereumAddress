2c9ecc3297e4e90ffafd9c2ee310f88e91cfac1c
humdx1 <ho@example.com>
2023-01-23 21:41:27 -0600

    ignore update cryptoswift lib

--- Pods/Target Support Files/Pods-EthereumAddress/Pods-EthereumAddress-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_EthereumAddress : NSObject
-@end
-@implementation PodsDummy_Pods_EthereumAddress
-@end
--- Pods/Target Support Files/Pods-EthereumAddressTests/Pods-EthereumAddressTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_EthereumAddressTests : NSObject
-@end
-@implementation PodsDummy_Pods_EthereumAddressTests
-@end
